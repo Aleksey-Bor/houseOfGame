1bb1e1ad888b3bd374d5e714e4ecef8bfc2a61a0
itka-school <itka.school@gmail.com>
2023-08-30 22:29:46 +0300

    Implemented function to style social media icons on hover

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 function changeLanguage(button) {
-  let language = button.querySelector('.header__button-language');
-  
-  if (language.textContent === 'En') {
-    language.textContent = 'Ru';
+  let language = button.querySelector(".header__button-language");
+
+  if (language.textContent === "En") {
+    language.textContent = "Ru";
   } else {
-    language.textContent = 'En';
+    language.textContent = "En";
   }
 }
+
+const links = document.querySelectorAll('.footer__social-link');
+
+links.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    const svg = link.querySelector('svg');
+    const path = svg.querySelector('path:first-of-type');
+    path.setAttribute('fill', '#9dadf2');
+  });
+
+  link.addEventListener('mouseout', () => {
+    const svg = link.querySelector('svg');
+    const path = svg.querySelector('path:first-of-type');
+    path.setAttribute('fill', '#478BF9');
+  });
+});
+
